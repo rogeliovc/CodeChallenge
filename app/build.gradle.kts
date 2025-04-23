a01713ef15009b868fbaa8ee74f6586ca56c2fa1
Rogelioovc <rv114576@.com>
2025-04-23 14:36:51 -0600

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.android.application)
+    // Plugin necesario para Firebase
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.codechallenge"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.codechallenge"
+        minSdk = 27
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    // Firebase Authentication
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    // Firebase Firestore
+    implementation("com.google.firebase:firebase-firestore:24.11.0")
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/example/codechallenge/MenuPrincipalActivity.java
@@ -0,0 +1,12 @@
+package com.example.codechallenge;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuPrincipalActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
